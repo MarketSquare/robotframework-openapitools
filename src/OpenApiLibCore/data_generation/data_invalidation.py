cb5b5310959e87817c90d549cb8d45b1ec502f3f
Robin Mackaij <r.a.mackaij@gmail.com>
2025-12-22 13:22:27 +0000

    Support for multiple OpenApiLibCore instances within a suite added.

--- src/OpenApiLibCore/data_generation/data_invalidation.py
+++ src/OpenApiLibCore/data_generation/data_invalidation.py
@@ -58,7 +58,7 @@ def _run_keyword(
 
 
 def _run_keyword(keyword_name: str, *args: object) -> object:
-    return run_keyword(keyword_name, *args)
+    return run_keyword(keyword_name, *args)  # pyright: ignore[reportArgumentType]
 
 
 def get_invalid_body_data(
